cdafbada8794a2e18ec3321a126fd60b4df2b44d
ginchandesu <142890093+ginchandesu@users.noreply.github.com>
2024-01-24 22:10:28 +0530

    update

--- dc/admin.py
+++ dc/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from.models import DC
 # Register your models here.
+@admin.register(DC)
+class DCadmin(admin.ModelAdmin):
+    list_display=['id','name','heroicname']
--- dc/models.py
+++ dc/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class DC(models.Model):
+    name=models.CharField(max_length=30)
+    heroicname=models.CharField(max_length=30)
+    
