8c6cbbe4aeb3558ff56e10f5d7c1fd6e7b4ea012
ginchandesu <142890093+ginchandesu@users.noreply.github.com>
2024-01-25 21:57:46 +0530

    update

--- dc/views.py
+++ dc/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from.models import DC
 # Create your views here.
 
 def superman(request):
-    con={'villan':'zod'}
-    return render(request,'dc/superman.html',con)
+    con=DC.objects.all()
+    return render(request,'dc/superman.html',{'con':con})
 
 def hulk(request):
     return render(request,'dc/hulk.html')
